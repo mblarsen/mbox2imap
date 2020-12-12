277164c93a48be2468bf781412473f4e6b8efbf6
Michael Bøcker-Larsen <m19n@pm.me>
2020-12-12 20:49:20 +0800

    docs: add minimal README

---
+++ README.md
@@ -0,0 +1,19 @@
+# mbox2imap
+
+A simple mbox to IMAP migration tool
+
+Lets you:
+
+- specify target mail box, defaults to \Archive
+- direct your own email(s) to \Sent
+- sets original send/receive date
+
+### Usage
+
+```shell
+cp Settings.example.toml Settings.toml
+```
+
+Update settings to your setup.
+
+Then run it!
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+//! A simple mbox to IMAP migration tool
+//!
+//! Lets you:
+//! - specify target mail box, defaults to \Archive
+//! - direct your own email(s) to \Sent
+//! - sets original send/receive date
+//!
+//! ## Usage
+//!
+//! ```shell
+//! cp Settings.example.toml Settings.toml
+//! ```
+//!
+//! Update settings to your setup.
+//!
+//! Then run it!
 mod mbox;
 mod myimap;
 
